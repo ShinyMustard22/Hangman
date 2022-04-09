6c46668070246b348e1f2ad7a83c1bdcfd78f35f
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-04-08 23:30:40 -0700

    Created the gallows.

---
+++ main.py
@@ -0,0 +1,15 @@
+def drawGallows():
+    print("  -----")
+    print("  |   |")
+    print("  |")
+    print("  |")
+    print("  |")
+    print("-----")
+
+
+def main():
+    drawGallows()
+
+
+if __name__ == "__main__":
+    main()
