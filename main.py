3e82171124eb87077d8f258e5943e32175faa469
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-04-09 13:31:38 -0700

    Now generates the word to be guessed.

--- main.py
+++ main.py
@@ -11,11 +11,44 @@ def drawGallows():
     print("-----")
 
 
+def file_len(filename):
+    with open(filename) as f:
+        for i, _ in enumerate(f):
+            pass
+    return i + 1
+
+
+def generateWord(letters):
+    if letters == 4:
+        word_file = open("words/fourLetterWords.txt", "r")
+
+    elif letters == 5:
+        word_file = open("words/fiveLetterWords.txt", "r")
+
+    elif letters == 6:
+        word_file = open("words/sixLetterWords.txt", "r")
+
+    else:
+        word_file = open("words/sevenLetterWords.txt", "r")
+
+    word = word_file.readlines()[random.randint(0, file_len(word_file.name) - 1)]
+    word_file.close()
+
+    word_list = list(word)
+    word_list.pop(len(word_list) - 1)
+
+    return word_list
+
+
 def main():
     man = Man()
-    rand = random.randint(4, 7)
+    letters = random.randint(4, 7)
 
-    word = ['_'] * rand
+    guessed_word = ['_'] * letters
+    actual_word = generateWord(letters)
+
+    print(guessed_word)
+    print(actual_word)
 
 
 if __name__ == "__main__":
