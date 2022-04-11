d0c0a32d934309d0c9186f8b7dcfdbf26cfbbaa0
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-04-11 15:22:50 -0700

    Changed the word_functions.py to a class that saves its own word.

--- man.py
+++ man.py
@@ -4,22 +4,22 @@ class Man:
 
     def addBodyPart(self):
         if self.stage == 1:
-            return # print head to console
+            return  # print head to console
 
         elif self.stage == 2:
-            return # print body to console
+            return  # print body to console
 
         elif self.stage == 3:
-            return # print left arm to console
+            return  # print left arm to console
 
         elif self.stage == 4:
-            return # print right arm to console
+            return  # print right arm to console
 
         elif self.stage == 5:
-            return # print left leg to console
+            return  # print left leg to console
 
         elif self.stage == 6:
-            return # print right leg to console
+            return  # print right leg to console
 
         self.stage += 1
 
