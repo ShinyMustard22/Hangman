34d51767c0a5304214c0f3e84d13340e6f09cf90
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-04-12 13:38:47 -0700

    Added function to convert a list into a string.

--- utility.py
+++ utility.py
@@ -3,3 +3,11 @@ def file_len(filename):
         for i, _ in enumerate(f):
             pass
     return i + 1
+
+
+def list_to_string(the_list):
+    string = ""
+    for i in the_list:
+        string = string + i
+
+    return string
